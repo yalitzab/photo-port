e62ade089146b4c22d07dfa81a8973fe116c71c7
yalitzab <yalitzabueno@gmail.com>
2020-12-30 14:16:02 -0800

    add about and navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // Create React App (CRA) comes with service worker support
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
